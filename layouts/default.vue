63c473f0b94bf653618e248fac3e21786bd4769a
Maksim <maksim.karpuk13910@gmail.com>
2022-08-23 20:34:35 +0300

    feature/MK-1

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="$style.main">
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header'
+
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style lang="scss" module>
+
+</style>
