139a273fa74ef1db704fab5342e563d194090e6e
Maksim <maksim.karpuk13910@gmail.com>
2022-08-30 20:40:13 +0300

    feature/MK-1

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,90 @@
 <template>
   <div>
-    <NuxtLink to="/"> Main</NuxtLink>
-    <NuxtLink to="/contacts">Contacts </NuxtLink>
-    <NuxtLink to="/delivery"> Delivery</NuxtLink>
-    <NuxtLink to="/promotions"> Promotions </NuxtLink>
+    <NuxtLink to="/">Main</NuxtLink>
+    <NuxtLink
+      v-for="link in links"
+      :key="link.id"
+      :to="link.path"
+      :class="[{ [$style.active]: link.isActive }]"
+      @click.native="changeLinkStatus(link.text)"
+    >
+      {{ link.text }}</NuxtLink
+    >
   </div>
 </template>
 
 <script>
 export default {
-  mounted(){
-    console.log(this.$route);
-  }
+  data() {
+    return {
+      links: [
+        {
+          isActive: false,
+          path: "/contacts",
+          id: 2,
+          text: "Contacts",
+        },
+        {
+          isActive: false,
+          path: "/delivery",
+          id: 3,
+          text: "Delivery",
+        },
+        {
+          isActive: false,
+          path: "/promotions",
+          id: 4,
+          text: "Promotions",
+        },
+      ],
+      fullPath: this.$route.path,
+    };
+  },
+  mounted() {
+    this.setActiveLink();
+  },
+  computed: {
+    getPath() {
+      return this.$route.path;
+    },
+  },
+  methods: {
+    setActiveLink() {
+      let activeLink = "";
+      const fullPath = this.$route.path;
+      switch (fullPath) {
+        case "/contacts":
+          activeLink = "Contacts";
+          break;
+        case "/delivery":
+          activeLink = "Delivery";
+          break;
+        case "/promotions":
+          activeLink = "Promotions";
+          break;
+        default:
+          activeLink = "";
+      }
+      this.changeLinkStatus(activeLink);
+    },
+    changeLinkStatus(text) {
+      console.log(text);
+      this.links.map((link) =>
+        link.text === text ? (link.isActive = true) : (link.isActive = false)
+      );
+    },
+  },
+  watch: {
+    getPath() {
+      this.setActiveLink();
+    },
+  },
 };
 </script>
+
+<style lang="scss" module>
+.active {
+  color: red;
+}
+</style>
 
